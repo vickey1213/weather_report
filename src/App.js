bbaf05f777ffae00806e3c8a298eaad042eb17b6
vickey1213 <vickey.kmr90@gmail.com>
2023-09-03 22:41:40 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <WeatherDashboard />
       </main>
       <footer className="app-footer">
-        <p>Copyright © 2023 Your Company Name</p>
+        <p>Copyright © 2023 Your Company Names</p>
       </footer>
     </div>
   );
